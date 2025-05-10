c1ae161d7e56f705645b75416104de53e61cfc79
Antonio Almeida - PCD <alfecjo@hotmail.com>
2025-05-10 18:00:31 -0300

    Update script.js

--- AWS-lab-S3/script.js
+++ AWS-lab-S3/script.js
@@ -1,14 +1,7 @@
 /*=============================================================
-    Authour URI: www.binarytheme.com
+    Authour URI: ACFA
     License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-
     ========================================================  */
-
 
     (function ($) {
         "use strict";
